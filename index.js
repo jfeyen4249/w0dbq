492f836941de5886ec99b32e7f9870f4163ba859
jfeyen4249 <76495851+jfeyen4249@users.noreply.github.com>
2022-02-21 09:31:55 -0600

    Beginning Code

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const multer = require("multer");
+const upload = multer({ dest: "./public/img" });
+const fs = require('fs');
+const dotenv = require('dotenv');
+const path = require('path');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+dotenv.config({ path: './.env'});
+const port = 80
+
+var connection = mysql.createConnection({
+    host     : process.env.db_host,
+    user     : process.env.db_user,
+    password : process.env.db_password,
+    database : process.env.db_database
+  });
+connection.connect();
+
+app.set('view engine', 'handlebars')
+app.set('view engine', 'hbs');
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+function authenticateTocken(req, res, nex) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader &&  authHeader.split(' ')[1]
+  if(token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.jwt_secret, (err, user) => {
+    if(err) return res.sendStatus(403)
+    req.user = user
+    nex()
+  })
+}
+
+
+// ****************************************
+// ****************************************
+// ****       Rendering Pages          ****
+// ****************************************
+// ****************************************
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
